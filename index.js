7d5ec6f680c3a23e80d80fa3886b4aa6cff132c2
Amey Morje <morje.amey@gmail.com>
2023-01-17 01:40:16 +0530

    App Updated for Posts CRUD operations

--- index.js
+++ index.js
@@ -6,23 +6,54 @@ const env = require('dotenv')
 const fs = require('fs')
 const path = require('path')
 
+const postRouter = require('./routes/postRouter');
+
+const Posts = require('./models/post');
+
+mongoose.connect('mongodb://localhost:27017/staging', () => {
+    console.log('connected to mongo')
+});
+
+
+
 const app = express()
 env.config()
-mongoose.connect(process.env.mongo_connection, () => {
-    console.log('connected to mongo')
-})
+// mongoose.connect(process.env.mongo_connection, () => {
+//     console.log('connected to mongo')
+// })
 
 // middleware
 app.use(express.json())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}));
 app.use(helmet())
 app.use(morgan('common', {
     stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 }))
 
-app.get('/', (req, res) => {
+app.use('/posts', postRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+  });
+
+  app.get('/', (req, res) => {
     res.send('We are at homepage!!!')
 })
 
-app.listen(8080, () => {
+  app.listen(8080, () => {
     console.log('Listening on port 8080!!!')
 })
+  
+  module.exports = app;
